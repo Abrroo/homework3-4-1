fda343d73a47d73a59f059f1392c0d7523ae39a0
Abrroo <akhmetov.ersyn@gmail.com>
2022-07-16 16:54:04 +0300

    gg

--- homework3-4-1/homework3-4-1.cpp
+++ homework3-4-1/homework3-4-1.cpp
@@ -1,20 +1,89 @@
-﻿// homework3-4-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cstring>
+#include <string>
+#include <fstream>
 
-#include <iostream>
+class adress
+{
+private:
+
+    std::string city, strt;
+    int bldg, appt;
+
+public:
+    adress(std::string city, std::string strt, int bldg, int appt)
+    {
+        this->city = city;
+        this->strt = strt;
+        this->bldg = bldg;
+        this->appt = appt;
+    }
+    adress() {}
+
+    std::string make_string()
+    {
+        std::string c = this->city;
+        std::string s = this->strt;
+        std::string b = std::to_string(this->bldg);
+        std::string a = std::to_string(this->appt);
+        return c + ", " + s + ", " + b + ", " + a;
+    }
+
+};
+
+int open_file(std::string path)
+{
+    std::ifstream file_read(path);
+    if (!file_read.is_open())
+    {
+        std::cout << "Can't open input file!" << std::endl;
+        return 1;
+    }
+    std::cout << "File is open" << std::endl;
+    return 0;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int N = 0;
+    int bldg = 0, appt = 0;
+    std::string city, strt;
+
+    std::ifstream file_read("in.txt");
+    if (!file_read.is_open())
+    {
+        std::cout << "Can't open input file!" << std::endl;
+        return 1;
+    }
+    std::cout << "Input file is open" << std::endl;
+    file_read >> N;
+    std::string* adr_arr = new std::string[N]();
+
+    for (int i = 0; i < N; i++)
+    {
+        file_read >> city;
+        file_read >> strt;
+        file_read >> bldg;
+        file_read >> appt;
+        adress adr(city, strt, bldg, appt);
+        adr_arr[i] = adr.make_string();
+    }
+
+    file_read.close();
+    std::ofstream file_write("out.txt");
+    if (!file_write.is_open())
+    {
+        std::cout << "Can't open output file!" << std::endl;
+        return 1;
+    }
+    std::cout << "Output file is open" << std::endl;
+    file_write << N << std::endl;
+    for (int i = N-1; i >= 0; i--)
+    {
+        file_write << adr_arr[i] << std::endl;
+    }
+    
+    std::cout << "Done!" << std::endl;
+
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
